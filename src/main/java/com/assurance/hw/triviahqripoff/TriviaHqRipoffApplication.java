5ea4bbf525a47efcca7f077facc38a2f58818a54
Jim Hamilton <hamilton@prismnet.com>
2020-08-17 16:47:54 -0700

    first

---
+++ src/main/java/com/assurance/hw/triviahqripoff/TriviaHqRipoffApplication.java
@@ -0,0 +1,13 @@
+package com.assurance.hw.triviahqripoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaHqRipoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaHqRipoffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assurance/hw/triviahqripoff/TriviaHqRipoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assurance.hw.triviahqripoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaHqRipoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
